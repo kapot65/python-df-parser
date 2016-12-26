4c49723a5fd73e2aade547f6407f55ea282d9c62
Vasilii Chernov <kapot65@gmail.com>
2016-12-26 15:48:55 +0300

    change version and readme

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # python-df-parser
 
 Parser for (dataforge)[http://npm.mipt.ru/dataforge/] envelope format
+
+## Installation 
+
+       pip3 install dfparser
+
--- setup.py
+++ setup.py
@@ -10,13 +10,13 @@ from distutils.core import setup
 setup(
   name = 'dfparser',
   packages = ['dfparser'],
-  version = '0.1',
+  version = '0.0.1',
   description = 'Parser for dataforge-envelope (http://npm.mipt.ru/dataforge/)'
   ' format.',
   author = 'Vasilii Chernov',
   author_email = 'kapot65@gmail.com',
   url = 'https://github.com/kapot65/python-df-parser',
-  download_url = 'https://github.com/kapot65/python-df-parser/tarball/0.1',
+  download_url = 'https://github.com/kapot65/python-df-parser/tarball/0.0.1',
   keywords = ['dataforge', 'parser'],
   classifiers = [],
 )
