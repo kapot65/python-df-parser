ecbcebfd0a863bb61fc8130b8963a8a46eb0d660
Vasilii <kapot65@gmail.com>
2017-01-14 21:39:38 +0300

    Fix binary data type handling.

--- setup.py
+++ setup.py
@@ -10,13 +10,13 @@ from distutils.core import setup
 setup(
   name = 'dfparser',
   packages = ['dfparser'],
-  version = '0.0.2',
+  version = '0.0.3',
   description = 'Parser for dataforge-envelope (http://npm.mipt.ru/dataforge/)'
   ' format.',
   author = 'Vasilii Chernov',
   author_email = 'kapot65@gmail.com',
   url = 'https://github.com/kapot65/python-df-parser',
-  download_url = 'https://github.com/kapot65/python-df-parser/tarball/0.0.2',
+  download_url = 'https://github.com/kapot65/python-df-parser/tarball/0.0.3',
   keywords = ['dataforge', 'parser'],
   classifiers = [],
 )
